67c6cba57d81ffc56230a72ca021a9ef8b639d7f
Raphael <rbellin.git@omachi.moe>
2022-12-01 10:49:15 +0100

    Project overhaul to use ASP
    This commit delete most of the old project to switch on a new one using ASP .NET6.0
    
    Adding EntityFramework
    
    We're adding EntityFramework as DB mapper

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_Jeu_Role.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Projet_Jeu_Role
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-				internal class Program
-				{
-								static void Main(string[] args)
-								{
-								}
-				}
+    app.UseExceptionHandler("/Home/Error");
 }
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
