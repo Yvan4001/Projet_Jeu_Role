1083b8256a157d078dd1934117ea506f04f008b5
Yvan Simon <114400983+Yvan4001@users.noreply.github.com>
2022-12-12 12:40:16 +0100

    Bdd Role

---
+++ Data/ProjetMVCContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetMVC.Models;
+
+namespace ProjetMVC.Data
+{
+    public class ProjetMVCContext : DbContext
+    {
+        public ProjetMVCContext (DbContextOptions<ProjetMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; } = default!;
+								public DbSet<Player> Player { get; set; } = default!;
+								public DbSet<Situation> Situation { get; set; } = default!;
+								public DbSet<Awnser> Awnser { get; set; } = default!;
+				}
+}
