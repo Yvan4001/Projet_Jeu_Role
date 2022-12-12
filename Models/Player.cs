551864516b4943639f92a1b561bcb81418fca5f0
Yvan Simon <114400983+Yvan4001@users.noreply.github.com>
2022-12-12 15:34:42 +0100

    Model

---
+++ Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Projet_Jeu_Role.Models
+{
+				public class Player
+				{
+								public int Id { get; set; }
+								public string Name { get; set; } = string.Empty;
+								public int UserId { get; set; }
+								public int SituationId { get; set; }
+				}
+}
---
+++ Models/Situation.cs
@@ -0,0 +1,10 @@
+﻿namespace Projet_Jeu_Role.Models
+{
+				public class Situation
+				{
+								public int Id { get; set; }
+								public string SituationName { get; set; } = string.Empty;
+								public string SituationDescription { get; set; } = string.Empty;
+								public List<Awnser> Awnsers { get; set; } = new List<Awnser>();
+				}
+}
